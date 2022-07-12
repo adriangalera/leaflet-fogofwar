7aa0ccd3bcd67a44b529faf7f8f82204d773a28d
Adrian Galera <adrian.galera.87@gmail.com>
2022-07-12 15:53:40 +0200

    Load/Save buttons

--- geojson-storage.js
+++ geojson-storage.js
@@ -15,4 +15,37 @@ function GeoJsonStorage() {
     }
 }
 
+const filePicketOpts = {
+    types: [
+        {
+            description: 'JSON Files',
+            accept: {
+                'application/json': ['.json']
+            }
+        },
+    ]
+};
 
+function Loader(storage, mask) {
+    const load = async () => {
+        [fileHandle] = await window.showOpenFilePicker(filePicketOpts);
+        const file = await fileHandle.getFile();
+        const content = await file.text();
+        const contentObj = JSON.parse(content)
+        storage.set(contentObj)
+        mask.setData(contentObj)
+    }
+    var btn = L.easyButton('fa-upload', load);
+    return btn;
+}
+
+function Saver(storage) {
+    const save = async () => {
+        const fileHandle = await window.showSaveFilePicker(filePicketOpts);
+        const writable = await fileHandle.createWritable();
+        await writable.write(JSON.stringify(storage.get()));
+        await writable.close();
+    }
+    var btn = L.easyButton('fa-save', save);
+    return btn;
+}
