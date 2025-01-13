cb5eb8b1ba198dfbf0bd0a5803c28cca91813241
Adrian Galera <adrian.galera.87@gmail.com>
2025-01-13 11:14:18 +0100

    update script

--- Makefile
+++ Makefile
@@ -15,7 +15,7 @@ minify:
 	cp /tmp/minified.json data/tracks.geojson
 
 find-cims-in-tracks:
-	wget https://www.agalera.eu/100cims/data/100cims/cims.json -O /tmp/100cims.json
-	wget https://www.agalera.eu/100cims/data/mendikat/cims.json -O /tmp/mendikat.json
+	wget -q https://www.agalera.eu/100cims/data/100cims/cims.json -O /tmp/100cims.json
+	wget -q  https://www.agalera.eu/100cims/data/mendikat/cims.json -O /tmp/mendikat.json
 	cp out-of-sync-gpx/* gpx/.
 	node scripts/find-cims-in-tracks.js
--- scripts/find-cims-in-tracks.js
+++ scripts/find-cims-in-tracks.js
@@ -1,6 +1,21 @@
 const fs = require('fs');
 const { QuadTreeNode } = require('../unique/quadtree');
 
+const checkCim = (referenceQt, item) => {
+    const lat = parseFloat(item.lat);
+    const lng = parseFloat(item.lng);
+    const fet = item.fet;
+    const presentInQuadTree = referenceQt.locationIsOnTree(lat, lng, 50);
+
+    if (!fet && presentInQuadTree) {
+        console.log(`Considered not done, but found in tracks! ${item.name} - ${item.link}`)
+    }
+
+    if (fet && !presentInQuadTree) {
+        console.log(`Considered done but not found in tracks! ${item.name} - ${item.link}`)
+    }
+}
+
 (async () => {
     // To read and deserialize it back:
     const quadtreeData = fs.readFileSync("scripts/quadtree.bin");
@@ -9,17 +24,9 @@ const { QuadTreeNode } = require('../unique/quadtree');
     const centCims = JSON.parse(fs.readFileSync("/tmp/100cims.json"));
     const mendikat = JSON.parse(fs.readFileSync("/tmp/mendikat.json"));
 
-    const cims = centCims.concat(mendikat);
- 
-    cims.forEach(item => {
-        const lat = parseFloat(item.lat);
-        const lng = parseFloat(item.lng);
-        const fet = item.fet;
-        const presentInQuadTree = referenceQt.locationIsOnTree(lat, lng, 200);
-
-        if (fet && !presentInQuadTree) {
-            console.log(`Cim ${item.name} not found in tracks! ${item.link}`)
-        }
-        
-    });
+    const cims = centCims//.concat(mendikat);
+    console.log("** Checking 100 cims ... **")
+    centCims.forEach(item => checkCim(referenceQt, item));
+    console.log("** Checking mendikat ... **")
+    mendikat.forEach(item => checkCim(referenceQt, item));
 })();
